8e357ad79b9e0873995e56662b314c26317af71a
piyushfromit <piyushfrommasai@gmail.com>
2025-01-26 15:16:14 +0530

    demo project of ritika

--- scripts/navbar.js
+++
@@ -1,9 +0,0 @@
-var navbar = document.getElementById("navbar")
-
-window.onscroll = function() {
-  if (window.pageYOffset > 0) {
-    navbar.classList.add('scrolled')
-  } else {
-    navbar.classList.remove('scrolled')
-  }
-}
